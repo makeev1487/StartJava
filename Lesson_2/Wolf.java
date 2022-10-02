150ce62e5a040f789679b4edb4d1b502c0c74f7f
Allam <makeev1487@gmail.com>
2022-10-02 22:35:29 +0700

    corrected Wolf, WolfTest

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,9 +1,9 @@
 public class Wolf {
-    String name;
-    char gender;
-    double weight;
-    int age;
-    String color;
+    private String name;
+    private char gender;
+    private double weight;
+    private int age;
+    private String color;
 
     public Wolf(String name, char gender, double weight, int age, String color) {
         this.name = name;
@@ -13,19 +13,63 @@ public class Wolf {
         this.color = color;
     }
 
-    void howl() {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+     public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("\nIncorrect age");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void howl() {
         System.out.println("Howling");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Sitting");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Running");
     }
 
-    void sleep () {
+    public void sleep() {
         System.out.println("Sleeping");
     }
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,12 +1,24 @@
 public class WolfTest {
     public static void main(String[] args) {
-        Wolf wolf = new Wolf("Rex", 'M', 40.6, 4, "gray");
+        Wolf wolf = new Wolf("Rex", 'M', 40.6, 6, "gray");
         
-        System.out.println("Name: " + wolf.name + "\n" +
-                "Gender: " + wolf.gender + "\n" +
-                "Weight: " + wolf.weight + "\n" +
-                "Age: " + wolf.age + "\n" +
-                "Color: " + wolf.color);
+        System.out.println("Name: " + wolf.getName() + "\n" +
+                "Gender: " + wolf.getGender() + "\n" +
+                "Weight: " + wolf.getWeight() + "\n" +
+                "Age: " + wolf.getAge() + "\n" +
+                "Color: " + wolf.getColor());
+
+        wolf.setName("Elbe");
+        wolf.setGender('F');
+        wolf.setWeight(35.6);
+        wolf.setAge(5);
+        wolf.setColor("White");
+
+        System.out.println("\nName: " + wolf.getName() + "\n" +
+                "Gender: " + wolf.getGender() + "\n" +
+                "Weight: " + wolf.getWeight() + "\n" +
+                "Age: " + wolf.getAge() + "\n" +
+                "Color: " + wolf.getColor());
 
         wolf.howl();
         wolf.sit();
