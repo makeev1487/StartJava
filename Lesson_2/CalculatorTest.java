c7fc180ecc8840a46e18211aed7b4793672830ed
Allam <makeev1487@gmail.com>
2022-10-04 23:34:12 +0700

    corrected CalculatorTest

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -5,60 +5,31 @@ public class CalculatorTest {
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
-            System.out.println("Enter a first number: ");
+            System.out.print("Enter a first number: ");
             int num1 = scanner.nextInt();
 
-            System.out.println("Enter a second number: ");
+            System.out.print("Enter a second number: ");
             int num2 = scanner.nextInt();
             scanner.nextLine();
 
-            System.out.println("Enter a math operation [+,-,*,/,^,%]: ");
+            System.out.print("Enter a math operation [+,-,*,/,^,%]: ");
             char mathOperation = scanner.nextLine().charAt(0);
 
             Calculator cal = new Calculator(num1,num2,mathOperation);
 
-            switch (cal.getMathOperation()) {
-                default:
-                    System.out.println("Math operation not supported");
-                    break;
-                case '+':
-                    System.out.println(cal.getNum1() + " " + cal.getMathOperation() + " " +
-                        cal.getNum2() + " = " + cal.sum());
-                    break;
-                case '-':
-                    System.out.println(cal.getNum1() + " " + cal.getMathOperation() + " " +
-                        cal.getNum2() + " = " + cal.subtract());
-                    break;
-                case '*':
-                    System.out.println(cal.getNum1() + " " + cal.getMathOperation() + " " +
-                        cal.getNum2() + " = " + cal.multiply());
-                    break;
-                case '/':
-                    if (num2 ==0) {
-                        System.out.println("Cannot be divide by zero");
-                    } else {
-                        System.out.println(cal.getNum1() + " " + cal.getMathOperation() + " " +
-                            cal.getNum2() + " = " + cal.divide());
-                    }
-                    break;
-                case '^':
-                    System.out.println(cal.getNum1() + " " + cal.getMathOperation() + " " +
-                        cal.getNum2() + " = " + cal.exponentiate());
-                    break;
-                case '%':
-                    if (num2 ==0) {
-                        System.out.println("Cannot be divide by zero");
-                    } else {
-                        System.out.println(cal.getNum1() + " " + cal.getMathOperation() + " " +
-                            cal.getNum2() + " = " + cal.remainder());
-                    }
-                    break;
+            if (((cal.getMathOperation() == '/' || cal.getMathOperation() == '%') && cal.getNum2() == 0)
+                    || !cal.hasMathOperation() {
+                cal.getCalculationResult();
+            } else {
+                System.out.println(cal.getNum1() + " " + cal.getMathOperation() + " " +
+                    cal.getNum2() + " = " + cal.getCalculationResult());
             }
-            System.out.println("Want to contine calculations [Y/N]: ");
+
+            System.out.print("Want to contine calculations [Y/N]: ");
             char answer = scanner.nextLine().charAt(0);
 
             if (answer == 'N' || answer == 'n') {
-                System.exit(0);
+                return;
             }
         }
     }
