0f16f35ca01365fd7b3b2a36b1185aff9c099973
Allam <makeev1487@gmail.com>
2022-10-06 23:07:21 +0700

    corrected CalculatorTest

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -6,23 +6,24 @@ public class CalculatorTest {
 
         while(true) {
             System.out.print("Enter a first number: ");
-            int num1 = scanner.nextInt();
+            double num1 = scanner.nextInt();
 
             System.out.print("Enter a second number: ");
-            int num2 = scanner.nextInt();
+            double num2 = scanner.nextInt();
             scanner.nextLine();
 
             System.out.print("Enter a math operation [+,-,*,/,^,%]: ");
             char mathOperation = scanner.nextLine().charAt(0);
 
-            Calculator cal = new Calculator(num1,num2,mathOperation);
+            Calculator cal = new Calculator(num1, num2, mathOperation);
 
-            if (((cal.getMathOperation() == '/' || cal.getMathOperation() == '%') && cal.getNum2() == 0)
-                    || !cal.hasMathOperation() {
-                cal.getCalculationResult();
+            double epsilon = 1.0e-10;
+
+            if ((mathOperation == '/' || mathOperation == '%') && Math.abs(num2) < epsilon) {
+                cal.calc();
             } else {
-                System.out.println(cal.getNum1() + " " + cal.getMathOperation() + " " +
-                    cal.getNum2() + " = " + cal.getCalculationResult());
+                System.out.println(num1 + " " + mathOperation + " " +
+                    num2 + " = " + cal.calc());
             }
 
             System.out.print("Want to contine calculations [Y/N]: ");
