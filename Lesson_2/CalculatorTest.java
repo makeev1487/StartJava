ee9232faf3dc717903de2987ac831a0a39837528
Allam <makeev1487@gmail.com>
2022-10-11 22:54:53 +0700

    fixed while from do-while in CalculatorTest

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,33 +4,32 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while(true) {
-            System.out.print("Enter a first number: ");
-            int num1 = scanner.nextInt();
+        String answer = "yes";
 
-            System.out.print("Enter a second number: ");
-            int num2 = scanner.nextInt();
-            scanner.nextLine();
+        do {
+            if ("yes".equals(answer)) {
+                System.out.print("Enter a first number: ");
+                int num1 = scanner.nextInt();
 
-            System.out.print("Enter a math operation [+,-,*,/,^,%]: ");
-            char mathOperation = scanner.nextLine().charAt(0);
+                System.out.print("Enter a second number: ");
+                int num2 = scanner.nextInt();
+                scanner.nextLine();
 
-            Calculator cal = new Calculator(num1, num2, mathOperation);
+                System.out.print("Enter a math operation [+,-,*,/,^,%]: ");
+                char mathOperation = scanner.nextLine().charAt(0);
 
-            System.out.println("Result: " + cal.calc());
+                Calculator cal = new Calculator(num1, num2, mathOperation);
 
-            while (true) {
-                System.out.print("Want to contine calculations [Y/N]: ");
-                char answer = scanner.nextLine().charAt(0);
-
-                if (answer == 'N' || answer == 'n') {
-                    return;
-                } else if (answer == 'Y' || answer == 'y') {
-                    break;
-                } else {
-                    System.out.println("Answer is not correct");
-                }
+                System.out.println("Result: " + cal.calc());
             }
-        }
+
+            System.out.print("Want to contine calculations [yes/no]: ");
+            answer = scanner.nextLine();
+
+            if (!"yes".equals(answer) && !"no".equals(answer)) {
+                System.out.println("Answer is not correct");
+            }
+
+        } while(!"no".equals(answer));
     }
 }
